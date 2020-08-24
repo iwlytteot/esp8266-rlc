ba6e52d8b461abd6984ef1c83cf3411b427b3771
Leo Tovaryš <64814989+iwlytteot@users.noreply.github.com>
2020-08-24 18:33:57 +0200

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,114 @@
+#include <ESP8266WiFi.h>
+
+#define RED 4
+#define GREEN 5
+#define BLUE 0
+
+char ssid[] = "";
+char password[] = "";
+int wl_status = WL_DISCONNECTED;
+
+WiFiServer server( 5432 );
+WiFiClient client;
+
+void setup() { 
+  pinMode( RED, OUTPUT );
+  pinMode( GREEN, OUTPUT );
+  pinMode( BLUE, OUTPUT );
+
+  
+  IPAddress staticIP(192, 168, 1, 200);
+  IPAddress subnet(255, 255, 255, 0);
+  IPAddress gateway(192, 168, 1, 1);
+  IPAddress dns(8, 8, 8, 8);
+  WiFi.config(staticIP, subnet, gateway, dns);
+  
+  do {
+    wl_status = WiFi.begin( ssid, password );
+    delay( 5000 );
+    analogWrite( RED, 700 );
+    delay( 300 );
+    analogWrite( RED, 0 );
+  } while ( wl_status != WL_CONNECTED );
+  
+  server.begin(); // Now this device is available on 192.168.1.51:5432 for TCP connection
+  analogWrite( RED, 700 );
+}
+
+void set_led( int red, int green, int blue ) {
+  analogWrite( RED, red );
+  analogWrite( GREEN, green );
+  analogWrite( BLUE, blue );
+}
+
+void check_available_client() {
+  if ( client )
+    return;
+  client = server.available();
+}
+
+// Pause in ms, limit as of RGB each color * 4
+void dimming( int *rgb ) {
+  int rgb_step[ 3 ];  
+  rgb_step[ 0 ] = rgb[ 3 ] - rgb[ 0 ];
+  rgb_step[ 1 ] = rgb[ 4 ] - rgb[ 1 ];
+  rgb_step[ 2 ] = rgb[ 5 ] - rgb[ 2 ];
+
+  int rgb_new[ 3 ] = { rgb[ 0 ], rgb[ 1 ], rgb[ 2 ] };
+  int lowest = min( min( abs( rgb_step[ 0 ] ), abs( rgb_step[ 1 ] ) ), abs( rgb_step[ 2 ] ) );
+  
+  for ( int i = 0; i < 3; ++i ) {
+    rgb_step[ i ] /= lowest;
+  }
+
+  int count = lowest;
+  while ( count-- > 0 && !client ) {
+    rgb_new[ 0 ] += rgb_step[ 0 ];
+    rgb_new[ 1 ] += rgb_step[ 1 ];
+    rgb_new[ 2 ] += rgb_step[ 2 ];
+    set_led( rgb_new[ 0 ], rgb_new[ 1 ], rgb_new[ 2 ] );
+    delay( rgb[ 7 ] );
+    check_available_client();
+  }
+
+  while ( count++ < lowest && !client ) {
+    rgb_new[ 0 ] -= rgb_step[ 0 ];
+    rgb_new[ 1 ] -= rgb_step[ 1 ];
+    rgb_new[ 2 ] -= rgb_step[ 2 ];
+    set_led( rgb_new[ 0 ], rgb_new[ 1 ], rgb_new[ 2 ] );
+    delay( rgb[ 7 ] );
+    check_available_client();
+  }
+}
+
+void handle_message( const String &line ) {
+  String part = "";
+  byte count = 0;
+  int rgb[8] = { 0 }; // | start: R G B (0, 1, 2 indexes), end: R G B (3, 4, 5 indexes), mode (6), speed (7) |
+  for ( char c : line ) {
+    if ( c == '.' ) {
+      rgb[ count ] = part.toInt();
+      if ( count < 6 )
+        rgb[ count ] *= 4;
+      ++count;
+      part = "";
+    } else {
+      part += c;
+    }  
+  }
+  if ( rgb[ 6 ] == 0 ) { set_led( rgb[0], rgb[1], rgb[2] ); }
+  if ( rgb[ 6 ] == 1 ) { 
+    while ( !client ) {      
+      dimming( rgb ); 
+    }
+  }
+}
+
+void loop() {
+  check_available_client();
+  if ( client.connected() ) {
+    String line = client.readStringUntil('\n');
+    client.stop();
+    handle_message( line );
+  }
+}
