cc03c7c88c4d9bd1e26c186dadd5fbead718d5cd
Leo Tovaryš <64814989+iwlytteot@users.noreply.github.com>
2020-08-27 14:33:42 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -12,12 +12,14 @@ WiFiServer server( 5432 );
 WiFiClient client;
 
 void setup() { 
+
+  Serial.begin( 115200 );
   pinMode( RED, OUTPUT );
   pinMode( GREEN, OUTPUT );
   pinMode( BLUE, OUTPUT );
 
   
-  IPAddress staticIP(192, 168, 1, 200);
+  IPAddress staticIP(192, 168, 1, 51);
   IPAddress subnet(255, 255, 255, 0);
   IPAddress gateway(192, 168, 1, 1);
   IPAddress dns(8, 8, 8, 8);
@@ -33,6 +35,7 @@ void setup() {
   
   server.begin(); // Now this device is available on 192.168.1.200:5432 for TCP connection
   analogWrite( RED, 700 );
+  
 }
 
 void set_led( int red, int green, int blue ) {
@@ -56,18 +59,38 @@ void dimming( int *rgb ) {
   int rgb_new[ 3 ] = { rgb[ 0 ], rgb[ 1 ], rgb[ 2 ] };
   int highest = max( max( abs( rgb_step[ 0 ] ), abs( rgb_step[ 1 ] ) ), abs( rgb_step[ 2 ] ) );
   bool state[ 3 ];
-  
+
+  int rgb_correction[ 3 ];
   for ( int i = 0; i < 3; ++i ) {
-    state[ i ]    = rgb_step[ 0 ] > 0;
-    rgb_step[ i ] = rgb_step[ i ] == 0 ? 255 : ( round( float( highest ) / abs( rgb_step[ i ] ) ) );
+    state[ i ] = rgb_step[ i ] < 0;
+    if ( rgb_step[ i ] == 0 ) {
+      rgb_correction[ i ] = 1025;
+      rgb_step[ i ] = 1025;
+    }
+    else {
+      int r_step = 10 * double( highest ) / abs( rgb_step[ i ] );
+      if ( r_step == 10 ) {
+        rgb_correction[ i ] = 1025;
+        rgb_step[ i ] = 1;
+      }
+      else {
+        rgb_correction[ i ] = 11 - ( r_step % 10 );
+        rgb_step[ i ] = ( r_step - ( r_step % 10 ) ) / 10;
+      }
+    }
   }
   
+  
+
+  int rgb_count[ 3 ] = { 0 };
   int count = 1;
   bool change = false;
   while ( !client ) {
     for ( int i = 0; i < 3; ++i ) {
       if ( count % rgb_step[ i ] == 0 ) {
-        if ( !change )
+        if ( ++rgb_count[ i ] % rgb_correction[ i ] == 0 )
+          continue;
+        if ( change )
           rgb_new[ i ] += state[ i ] ? 1 : -1;
         else
           rgb_new[ i ] += state[ i ] ? -1 : 1;
@@ -77,9 +100,17 @@ void dimming( int *rgb ) {
     
     delay( rgb[ 7 ] );
     
-    if ( count > highest && change )
+    if ( count >= highest && change )
       break;
-    else if ( count > highest ) {
+    else if ( count >= highest ) {
+      Serial.print( "From ");
+      Serial.print( rgb[ 1 ] );
+      Serial.print( " to ");
+      Serial.print( rgb_new[ 1 ] );
+      Serial.print( " of highest: " );
+      Serial.print( highest );
+      Serial.print( " state: " );
+      Serial.println( state[ 1 ] );
       change = true; count = 1;
     } 
     else
@@ -105,18 +136,15 @@ void handle_message( const String &line ) {
     }  
   }
   if ( rgb[ 6 ] == 0 ) { set_led( rgb[0], rgb[1], rgb[2] ); }
-  if ( rgb[ 6 ] == 1 ) { 
-    while ( !client ) {      
-      dimming( rgb ); 
-    }
-  }
+  if ( rgb[ 6 ] == 1 ) { while ( !client ) dimming( rgb ); }
 }
 
 void loop() {
+  
   check_available_client();
   if ( client.connected() ) {
     String line = client.readStringUntil('\n');
     client.stop();
     handle_message( line );
-  }
+  } 
 }
